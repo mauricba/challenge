3e32ca80d02f8e532a8b96ed18259a3e64a2ebc4
mauricba <mauricba05@gmail.com>
2026-03-11 13:04:46 -0300

    Add login test scenario for valid credentials

---
+++ LoginTest.kt
@@ -0,0 +1,7 @@
+Scenario 1: Login valid
+
+Gvien the URL https://the-internet.herokuapp.com/login 
+And the $valid credentials for the login 
+When I enter the credentials 
+Then I see the Secure area page 
+And I see the logged message
